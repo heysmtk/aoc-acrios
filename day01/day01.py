8f1a15335703ec8d853b9e73bcee425fe264dfba
heysmtk <smtktom@gmail.com>
2025-03-19 21:14:12 +0100

    add comments

--- day01/day01.py
+++ day01/day01.py
@@ -3,10 +3,14 @@
 # Otevírá a čte ze souboru, kde jsou data a ty ukládá do listu
 with open("day01/large_data.txt", "r") as file:
     content = file.readlines()
-  
+
+# For cykl, který vytáhne položku po položce z listu a pracuje s ní dál
 total = 0  
 for item in content:
-    numbers = [char for char in item if char.isdigit()]  # Listová komprehenze, která mi vyfiltruje čísla do listů
-    total += int(numbers[0] + numbers[-1])
+    # List comprehension - Vyfiltrování čísel z jednotlivých položek dat
+    numbers = [char for char in item if char.isdigit()]
+    total += int(numbers[0] + numbers[-1])  # Sečtění první a poslední číslice z položky dat a uložení do proměnné "total"
 
 print(total)
+
+# PART 2

be13f98fc2d2039a47972e6d4f4ff217c7b4cce3
heysmtk <smtktom@gmail.com>
2025-03-24 22:04:52 +0100

    add part two of day 6

--- day06/day06.py
+++ day06/day06.py
@@ -3,23 +3,37 @@
 times = [54, 81, 70, 88]
 distances = [446, 1292, 1035, 1007]
 
-# Prázdný list pro budoucí data výsledků
-race_results = []
-
-# For cyklus, který mi "zipuje" times a distance hodnoty
-for time, distance in zip(times, distances):
-    chance_of_wins = 0  # Připravená prázdná hodnota
-
-    # Výpočet času a vzádelnosti pro každý závod 
-    for active_time in range(1, time):
-        speed = active_time
-        post_distance = speed * (time - active_time)
-
-        # Porovnání ujeté vzdálenosti vs. dané vzdálenosti
-        if post_distance > distance:
-            chance_of_wins += 1  # Pokud je větší než požadovaná, navýší se o jednu jednotku
+# Funkce, která vypočítá čas a vzálenost k závodům (pro part 1 i 2)
+def race_calc(times, distances):
+    results = []  # Připravený list pro výsledky
     
-    # Uložení iterace (závodu) do listu
-    race_results.append(chance_of_wins)
+    for time, distance in zip(times, distances):
+        chance_of_wins = 0  # Připravená prázdná proměnná pro přičítání možností na "výhru"
 
-print(race_results)
+        # Výpočet času a vzádelnosti pro každý závod 
+        for active_time in range(1, time):
+            speed = active_time
+            post_distance = speed * (time - active_time)
+
+            # Porovnání ujeté vzdálenosti vs. dané vzdálenosti
+            if post_distance > distance:
+                chance_of_wins += 1 
+                
+        # Uložení iterace (závodu) do listu
+        results.append(chance_of_wins)
+    return results
+
+# Uložení funkce s argumenty do proměnné k vypsání
+race_results = race_calc(times, distances)
+print(f"Part One Results: {race_results}")
+
+
+# ---PART TWO---
+
+# Převedení původních listů s daty na stringy a spojení dohromady v listu (čili výstup je opět list)
+times_full = [int("".join(map(str, times)))]
+distances_full = [int("".join(map(str, distances)))]
+
+# Změna parametrů pro již existující funkci pro výpočet partu 2
+full_race_result = race_calc(times_full, distances_full)
+print(f"Part Two Results: {full_race_result}")
